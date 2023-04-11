d8d361bdabe7230ec49b4c20fdb46a3a7f8acf3c
LeeKyungwon <113881477+gracelee5@users.noreply.github.com>
2023-04-11 22:30:50 +0900

    Add files via upload

---
+++ Chapter 4/ADS4_1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<long long> memo;
+
+long long fibo(int N){
+    if(N==0) return 0;
+    else if(N==1) return 0;
+    else if(N==2) return 1;
+
+    if(memo[N] != -1) return memo[N];
+
+    return memo[N] = fibo(N-1) + fibo(N-2) + fibo(N-3);
+}
+
+int main(){
+    memo.assign(50,-1);
+    int N;
+    cin>>N;
+    cout<<fibo(N)<<endl;
+}
